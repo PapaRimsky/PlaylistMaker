c303d44f0172576f19a691a2cfab7a1ab9e95203
PapaRimsky <khvishuk@yandex.ru>
2023-07-06 22:52:32 +0500

    Finish

--- app/src/main/java/com/paparimsky/playlistmaker2/SearchActivity.kt
+++ app/src/main/java/com/paparimsky/playlistmaker2/SearchActivity.kt
@@ -1,11 +1,68 @@
 package com.paparimsky.playlistmaker2
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val imageButtonBack = findViewById<ImageButton>(R.id.backToMainFromSearch)
+        val editText = findViewById<EditText>(R.id.editText)
+        val display = findViewById<LinearLayout>(R.id.display)
+        val cross = findViewById<ImageView>(R.id.cross)
+        editText.requestFocus()
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+        if (savedInstanceState != null) {
+            editText.setText(savedInstanceState.getString(SEARCH_TEXT,""))
+        }
+        imageButtonBack.setOnClickListener {
+            val displayMain = Intent(this, MainActivity::class.java)
+            startActivity(displayMain)
+            finish()
+        }
+        display.setOnClickListener {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
+            editText.clearFocus()
+        }
+        cross.setOnClickListener {
+            editText.setText("")
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                cross.visibility = clearCrossVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Toast.makeText(this@SearchActivity, "Поиск...", Toast.LENGTH_SHORT).show()
+            }
+        }
+        editText.addTextChangedListener(simpleTextWatcher)
+    }
+    private fun clearCrossVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val editText = findViewById<EditText>(R.id.editText)
+        outState.putString(SEARCH_TEXT,editText.text.toString())
     }
 }
--- app/src/main/java/com/paparimsky/playlistmaker2/SettingsActivity.kt
+++ app/src/main/java/com/paparimsky/playlistmaker2/SettingsActivity.kt
@@ -1,19 +1,50 @@
 package com.paparimsky.playlistmaker2
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val buttonSearch = findViewById<Button>(R.id.backToMain)
-        buttonSearch.setOnClickListener{
+        val imageButtonBack = findViewById<ImageButton>(R.id.backToMainFromSettings)
+        val viewToShare = findViewById<LinearLayout>(R.id.toShare)
+        val viewToSupport = findViewById<LinearLayout>(R.id.toSupport)
+        val viewToAgreement = findViewById<LinearLayout>(R.id.toAgreement)
+        val linearTheme = findViewById<LinearLayout>(R.id.changeTheme)
+        val switchTheme = findViewById<Switch>(R.id.switchTheme)
+        imageButtonBack.setOnClickListener{
             val displayMain = Intent(this, MainActivity::class.java)
             startActivity(displayMain)
             finish()
+        }
+        viewToShare.setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer")
+            startActivity(shareIntent)
+        }
+        viewToSupport.setOnClickListener{
+            val supIntent = Intent(Intent.ACTION_SENDTO)
+            supIntent.data = Uri.parse("mailto:")
+            supIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("khvishuk@yandex.ru"))
+            supIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            supIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            startActivity(supIntent)
+        }
+        viewToAgreement.setOnClickListener{
+            val agreeIntent = Intent(Intent.ACTION_VIEW)
+            agreeIntent.data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(agreeIntent)
+        }
+        linearTheme.setOnClickListener{
+            switchTheme.isChecked= !switchTheme.isChecked
         }
     }
 }
