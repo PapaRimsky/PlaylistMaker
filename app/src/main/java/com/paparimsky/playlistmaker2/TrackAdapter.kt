994c9dd7432c503718ca8ed236a86c1a19777131
PapaRimsky <khvishuk@yandex.ru>
2023-08-04 12:24:55 +0500

    советы ревьюера

--- app/src/main/java/com/paparimsky/playlistmaker2/TrackAdapter.kt
+++ app/src/main/java/com/paparimsky/playlistmaker2/TrackAdapter.kt
@@ -1,18 +1,16 @@
 package com.paparimsky.playlistmaker2
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter(
     private val tracks: List<Track>,
-    private val tContext: Context
 ) : RecyclerView.Adapter<TrackViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
-        return TrackViewHolder(view,tContext)
+        return TrackViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
--- app/src/main/java/com/paparimsky/playlistmaker2/TrackViewHolder.kt
+++ app/src/main/java/com/paparimsky/playlistmaker2/TrackViewHolder.kt
@@ -9,28 +9,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView) {
-    private val tContext: Context = context
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackImage: ImageView = itemView.findViewById(R.id.track_image)
     private val trackName: TextView = itemView.findViewById(R.id.track_name)
     private val artistName: TextView = itemView.findViewById(R.id.artist_name)
     private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+    private val layoutRadius = 2f
 
     fun bind(model: Track) {
         Glide.with(itemView)
             .load(model.artworkUrl100)
             .placeholder(R.drawable.note)
             .centerCrop()
-            .transform(RoundedCorners(dpToPx(2f, tContext)))
+            .transform(RoundedCorners(dpToPx(layoutRadius, itemView.context)))
             .into(trackImage)
         trackName.text = model.trackName
         artistName.text = model.artistName
         trackTime.text = model.trackTime
     }
+
     private fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
             dp,
-            context.resources.displayMetrics).toInt()
+            context.resources.displayMetrics
+        ).toInt()
     }
 }
